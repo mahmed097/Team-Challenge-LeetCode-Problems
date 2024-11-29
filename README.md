a86fb7f99d5244a0e0e56d63cb827ac646555285
Umukulthum <kanyinsolaumukulthum@gmail.com>
2024-11-29 13:51:14 -0500

    Updated Readme.md file with description of Merged Lists

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LeetCode Algorithm Challenges
 
-This project solves Algorithm Challenges using Java.
+This repository contains solutions to various algorithmic challenges from LeetCode, implemented in Java. It serves as a resource for learning, practicing, and mastering data structures and algorithms through practical coding exercises. The project is broken down into different packages with each containing a Java file named after the corresponding LeetCode problem.
 
 Challenge 1: [Average Salary Excluding the Minimum and Maximum Salary](https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/description/)
 
@@ -11,6 +11,24 @@ Challenge 3: [Missing Number](https://leetcode.com/problems/missing-number/descr
 Challenge 4: [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/)
 
 Challenge 5: [Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/description/)
+
+This algorithm takes two Array lists, list1 and list2, where the elements in each list are sorted in non-decreasing order, the task is to merge them into a single Array list such that the merged list remains sorted. The algorithm achieves this by comparing elements from both lists iteratively.
+
+Constraints:
+
+- The number of elements in both lists is in the range [0, 50].
+- -100 <= element <= 100
+- Both list1 and list2 are sorted in non-decreasing order.
+
+Examples:
+
+- Input: list1 = [1,2,4], list2 = [1,3,4] Output: [1,1,2,3,4,4]
+- Input: list1 = [], list2 = [] Output: []
+- Input: list1 = [], list2 = [0] Output: [0]
+
+## Pseudocode
+
+[Challenges Pseudocodes](https://docs.google.com/document/d/1Xv0JEAQ_iV23AVGZ-_7Qs5zuD6jVJuTI5lfFbbY_c1I/edit?tab=t.0)
 
 ## Authors
 
--- challenge1/AverageSalary.java
+++ challenge1/AverageSalary.java
@@ -1,31 +1,35 @@
+package challenge1;
+
 class AverageSalary {
-    public double average(int[] salary) {
+	public double average(int[] salary) {
 
-        double sum = 0;
-        int min = (int)Math.pow(10, 7); // Initializing minimum salary and setting it to a number outside our constraints (Which was 1000 <= salary[i] <= 10^6)
-        int max = 999; // Initializing maximum salary and setting it to a number outside our constraints (Which was 1000 <= salary[i] <= 10^6)
+		double sum = 0;
+		int min = (int) Math.pow(10, 7); // Initializing minimum salary and setting it to a number outside our
+											// constraints (Which was 1000 <= salary[i] <= 10^6)
+		int max = 999; // Initializing maximum salary and setting it to a number outside our
+						// constraints (Which was 1000 <= salary[i] <= 10^6)
 
-        for(int i = 0; i<salary.length; i++){
-          
-            if(salary[i]>max){ // Checking for maximum number
-                max = salary[i]; // If maximum salary, store it in max
-            }
-             if(salary[i]<min){ // Checking for minimum number
-                min = salary[i]; // If minimum salary, store it in min
-            }
+		for (int i = 0; i < salary.length; i++) {
 
-            sum += salary[i]; // Adding all the salaries togeth
+			if (salary[i] > max) { // Checking for maximum number
+				max = salary[i]; // If maximum salary, store it in max
+			}
+			if (salary[i] < min) { // Checking for minimum number
+				min = salary[i]; // If minimum salary, store it in min
+			}
 
-        }
+			sum += salary[i]; // Adding all the salaries togeth
 
-        return (sum-max-min)/(salary.length - 2);  // Returning the average salary after removing max and min
-        
-    }
+		}
 
-    public static void main(String[] args) {
-        // Test output
-        AverageSalary s = new AverageSalary(); 
-        System.out.println(s.average(new int[]{4000,3000,1000,2000}));  
-        System.out.println(s.average(new int[]{1000,2000,3000})); 
-    }
+		return (sum - max - min) / (salary.length - 2); // Returning the average salary after removing max and min
+
+	}
+
+	public static void main(String[] args) {
+		// Test output
+		AverageSalary s = new AverageSalary();
+		System.out.println(s.average(new int[] { 4000, 3000, 1000, 2000 }));
+		System.out.println(s.average(new int[] { 1000, 2000, 3000 }));
+	}
 }
